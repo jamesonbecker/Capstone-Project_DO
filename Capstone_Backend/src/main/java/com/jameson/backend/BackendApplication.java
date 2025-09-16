76f25d894634fd6b8d1e8851c2a778263a7b03bd
Jameson <bjameson9213@gmail.com>
2025-09-16 17:34:06 -0500

    Changed Port

--- Capstone_Backend/src/main/java/com/jameson/backend/BackendApplication.java
+++ Capstone_Backend/src/main/java/com/jameson/backend/BackendApplication.java
@@ -19,7 +19,7 @@ public class BackendApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://jamesonbecker.com:4200")
+                        .allowedOrigins("https://jamesonbecker.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
